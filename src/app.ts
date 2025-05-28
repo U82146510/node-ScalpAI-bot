9995ee3de110162ac22820cdfa6791ad90af4917
U82146510 <U82146510@protonmail.com>
2025-05-28 19:01:56 +0100

    adding logger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import {bot} from './Telegram/bot.ts';
+import {logger} from './logger/logger.ts';
 
 async function start():Promise<void>{
     try {
-        
+        logger.info('Bot On');
         await bot.start();
     } catch (error) {
         console.error(error);
